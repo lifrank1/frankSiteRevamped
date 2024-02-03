96cbb407bc548cf5eb8d722960edb3a0d9dd0be3
lifrank1 <frank.li.865985@gmail.com>
2024-02-02 23:12:41 -0500

    updated experience

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,53 +1,71 @@
-import { Box, Container, Grid, Stack, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import PropTypes from "prop-types";
 
 // Changes to the expereinces array affects the experiences view
 const experiences = [
   {
-    "title": "Title",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "date": "MM/DD/YY",
+    "JP Morgan Chase": "Software Engineering Intern",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "MM/DD/YY",
   },
   {
-    "title": "Title",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "date": "MM/DD/YY",
+    Fiserv: "Software Engineering Intern",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "MM/DD/YY",
   },
   {
-    "title": "Title",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "date": "MM/DD/YY",
+    title: "Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "MM/DD/YY",
   },
   {
-    "title": "Title",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "date": "MM/DD/YY",
+    title: "Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    date: "MM/DD/YY",
   },
 ];
 
-function Experience({title, description, date, flip}) {
+function Experience({ title, description, date, flip }) {
   // useTheme() gives this component the theme. We will use it to get the primary color
   const theme = useTheme();
   const color = theme.palette.primary[500];
 
   return (
-    <Grid container direction={flip?"row-reverse":"row"} position={'relative'}>
+    <Grid
+      container
+      direction={flip ? "row-reverse" : "row"}
+      position={"relative"}
+    >
       <Grid item xs={6}>
         {/*
           Box component is just a fancy div with helper attributes
         */}
-        <Box {...{[flip?"ml":"mr"]: "20px"}}>
-          <Typography variant="h5" mb={4} align={flip?"left":"right"}>
+        <Box {...{ [flip ? "ml" : "mr"]: "20px" }}>
+          <Typography variant="h5" mb={4} align={flip ? "left" : "right"}>
             {title}
           </Typography>
-          <Typography variant="body1" align={flip?"left":"right"}>
+          <Typography variant="body1" align={flip ? "left" : "right"}>
             {description}
           </Typography>
         </Box>
       </Grid>
       <Grid item xs={6}>
-        <Stack {...{[flip?"mr":"ml"]: "20px"}} alignItems={flip?"flex-end":"flex-start"}>
-          <Box sx={{backgroundColor: color, width: '150px'}} borderRadius={6}>
+        <Stack
+          {...{ [flip ? "mr" : "ml"]: "20px" }}
+          alignItems={flip ? "flex-end" : "flex-start"}
+        >
+          <Box sx={{ backgroundColor: color, width: "150px" }} borderRadius={6}>
             <Typography variant="h6" color={"white"} align="center">
               {date}
             </Typography>
@@ -57,16 +75,17 @@ function Experience({title, description, date, flip}) {
       {/*
         You can apply styles to most Material-Ui components easily using sx 
       */}
-      <Box sx={{
-        width: "25px",
-        height: "25px",
-        backgroundColor: color,
-        borderRadius: '100%',
-        position: 'absolute',
-        left: '50%',
-        top: '5px',
-        transform: "translateX(-50%)",
-        zIndex: 1
+      <Box
+        sx={{
+          width: "25px",
+          height: "25px",
+          backgroundColor: color,
+          borderRadius: "100%",
+          position: "absolute",
+          left: "50%",
+          top: "5px",
+          transform: "translateX(-50%)",
+          zIndex: 1,
         }}
       />
     </Grid>
@@ -77,36 +96,32 @@ Experience.propTypes = {
   description: PropTypes.string,
   date: PropTypes.string,
   flip: PropTypes.bool,
-}
+};
 
 function ExperiencesPage() {
   return (
     <Container>
-      <Typography variant="h2">
-        Experiences
-      </Typography>
-      <Box position={'relative'} mb={30}>
-        <Stack spacing={20} sx={{mt: 10}}>
-          {
-            experiences.map(({title, description, date}, i) => (
-              <Experience
-                title={title}
-                description={description}
-                date={date}
-                flip={(i%2)==1}
-                key={i}
-              />
-            ))
-          }
+      <Typography variant="h2">Experiences</Typography>
+      <Box position={"relative"} mb={30}>
+        <Stack spacing={20} sx={{ mt: 10 }}>
+          {experiences.map(({ title, description, date }, i) => (
+            <Experience
+              title={title}
+              description={description}
+              date={date}
+              flip={i % 2 == 1}
+              key={i}
+            />
+          ))}
         </Stack>
         <Box
           sx={{
-            position: 'absolute',
-            left: '50%',
-            top: '5px',
-            bottom: '0px',
+            position: "absolute",
+            left: "50%",
+            top: "5px",
+            bottom: "0px",
             width: "5px",
-            backgroundColor: 'gray',
+            backgroundColor: "gray",
             transform: "translateX(-50%)",
           }}
         />
